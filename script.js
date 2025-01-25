84e67ac8ce90952cc83bf4abf3f8f3f2d09ed6b7
suraj165d <surajdey9830@gmail.com>
2025-01-25 16:32:52 +0530

    initial

---
+++ script.js
@@ -0,0 +1,72 @@
+function sendBTC() {
+    // Get the name
+    var name = "BTC-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-btc.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+
+
+function sendDOGE() {
+    // Get the name
+    var name = "DOGE-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendSHB() {
+    // Get the name
+    var name = "SHIB-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-shb.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendBNC() {
+    // Get the name
+    var name = "BNB-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-predictor-bnb.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendSOL() {
+    // Get the name
+    var name = "SOL-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-predictor-sol.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendNEO() {
+    // Get the name
+    var name = "NEO-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-predictor-neo.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendLITE() {
+    // Get the name
+    var name = "LTC-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-predictor-lite.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendPOL() {
+    // Get the name
+    var name = "MATIC-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-predictor-polygon.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendETH() {
+    // Get the name
+    var name = "ETH-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit-predictor-eth.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
+function sendMON() {
+    // Get the name
+    var name = "XMR-USD";
+
+    // Redirect to the receiver page with the name as a parameter
+    window.open('https://switch-to-bit.streamlit.app/?name=' + encodeURIComponent(name), '_blank');
+}
